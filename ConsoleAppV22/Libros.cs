007759115a96e78a6b5cb288031315ba08dfc4a4
José Alberto García <jagarcias1978@gmail.com>
2022-09-20 09:54:27 +0200

    Propiedades en C#

---
+++ ConsoleAppV22/Libros.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppV22
+{
+    public class Libros
+    {
+		private int int_edad;
+
+		public int Edad
+		{
+			get { return int_edad; }
+			set { int_edad = value; }
+		}
+
+		private string str_nombre;
+
+		public string Nombre
+		{
+			get { return str_nombre; }
+			set { str_nombre = value; }
+		}
+
+
+	}
+}
--- ConsoleAppV22/Program.cs
+++ ConsoleAppV22/Program.cs
@@ -1,7 +1,14 @@
-﻿internal class Program
+﻿using ConsoleAppV22;
+using System.Collections;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
         Console.WriteLine("Hola, Mundo!");
+        List<Libros> lst_libros = new List<Libros>();
+        Libros obj = new Libros();
+        lst_libros.Add(obj);
+        Console.WriteLine("El número de elementos de ls lista es: " + lst_libros.Count);
     }
 }
